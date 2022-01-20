13cbda03f5104d598396d3c525b65a7978c14ced
Alex Staelens <Alex.Staelens@vu.com>
2022-01-20 15:19:37 -0600

    added input for rate, cast inputs to float variables, multiply and print gross pay

--- payCalculator.py
+++ payCalculator.py
@@ -3,11 +3,15 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours:")
-    
+    rate = input("Enter Rate:")
+    flHrs = float(hrs)
+    flRate = float(rate)
+    pay = flHrs*flRate
+    print(pay)
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+calculatePay()
